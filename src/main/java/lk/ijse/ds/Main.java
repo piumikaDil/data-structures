a3e48f8d9617311b063dbe9f4dc985ca469ba4db
piumikaDil <piumikaDil@gmail.com>
2023-11-22 10:30:11 +0530

    push and peak

---
+++ src/main/java/lk/ijse/ds/Main.java
@@ -0,0 +1,25 @@
+package lk.ijse.ds;
+//Author : Piumika Dilshan
+//Connect : piumikadil@gmail.com
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        int peak = stack.peak();
+        System.out.println("peak : "+peak);
+
+        stack.push(40);
+        int peak1 = stack.peak();
+        System.out.println("peak : "+peak1);
+
+        stack.push(50);
+        int peak2 = stack.peak();
+        System.out.println("peak : "+peak2);
+
+    }
+}
---
+++ src/main/java/lk/ijse/ds/Stack.java
@@ -0,0 +1,21 @@
+package lk.ijse.ds;
+//Author : Piumika Dilshan
+//Connect : piumikadil@gmail.com
+
+public class Stack {
+    private final int[] elementData;
+    private int top;
+
+    public Stack(int initialCapacity) {
+        elementData = new int[initialCapacity];
+        top = -1;
+    }
+
+    public void push(int data) {
+        elementData[++top] = data;
+    }
+
+    public int peak() {//top
+        return elementData[top];
+    }
+}
